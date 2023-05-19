0ef8ab927aab35320aac255aaccd638209250eff
spencerWind <swind03@gmail.com>
2023-05-19 02:27:27 -0400

    continued work on navbar component

--- client/src/components/Navigation/NavItem.jsx
+++ client/src/components/Navigation/NavItem.jsx
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom"
 
-const NavItem = ({ value, destination }) => {
+const NavItem = ({ value, destination, }) => {
     return (
         <Link to ={destination}>
-            <li className="">{value}</li>
+            <li className="text-slate-400 relative font-light">
+                {value}
+                <span className="absolute w-6 h-[1.5px] bg-primary -bottom-1 left-1/2 -translate-x-1/2"></span>
+                </li>
         </Link>
     );
 };
